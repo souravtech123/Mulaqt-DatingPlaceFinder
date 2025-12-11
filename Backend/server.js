a162a11f23b5e3768001f7366f0a989ce4b47f26
Sourav Suman <Install 'code' command in PATH>
2025-12-11 22:03:03 +0530

    fourth

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
-require('dotenv').config();
+const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
