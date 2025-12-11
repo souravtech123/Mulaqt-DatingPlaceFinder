f977a0d7be991c9696d8f18c44c9e2370d11bf67
Sourav Suman <souravsuman846@gmail.com>
2025-12-11 21:54:00 +0530

    third

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
-import dotenv from "dotenv";
+require('dotenv').config();
 dotenv.config();
 
 const app = express();
